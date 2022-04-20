7e298f7a02bf86c8061bf11e48015fa754509ba0
Hicaro_Brasil <91800494+brasilhicaro@users.noreply.github.com>
2022-04-20 14:32:48 -0300

    Update grafo.py

--- grafo.py
+++ grafo.py
@@ -5,20 +5,23 @@ import pylab
 G = nx.Graph()
 
 G.add_edges_from([('Monteiro', 'Sertânia')])
-G.add_edges_from([('Monteiro' , 'Sumé'), 
-    ('Monteiro','Camalaú'), ('Monteiro', 'Zabelê')])
-G.add_edges_from(
-    [('São Sebastião do Umbuzeiro' , 'Zabelê'), ('Serra Branca','Sumé'),
-    ('Congo', 'Sumé'), ('São José do Egito','Ouro Velho'), ('Ouro Velho', 'Prata'),
-    ('Prata', 'Monteiro')])
+G.add_edges_from([('Monteiro' , 'Sumé'), ('Monteiro','Camalaú'), ('Monteiro', 'Zabelê')])
+G.add_edges_from([('São Sebastião do Umbuzeiro' , 'Zabelê')
+,('Serra Branca','Sumé'),('Congo', 'Sumé'), ('São José do Egito','Ouro Velho')
+, ('Ouro Velho', 'Prata'), ('Prata', 'Monteiro')]) 
 #Monteiro para Recife
-G.add_edges_from(
-    [("Congo", "Jataúba"), ('Jataúba', 'Toritama'),('Toritama','Caruaru'), ('Caruaru', 'Recife')]
-)
-val_map = {'Monteiro': 0.6,
-           'Sertânia': 0.9,
-           'Camalaú': 0.8,
-           'Sumé': 0.7,
+G.add_edges_from([("Congo", "Jataúba"), ('Jataúba', 'Toritama')
+, ('Toritama','Caruaru'), ('Caruaru', 'Recife')])
+#Monteiro para Recife, indo por JP
+G.add_edges_from([('Sumé','São José do Cariri'), ('São José do Cariri', 'Campina Grande')
+, ('Campina Grande','João Pessoa'), ('João Pessoa','Goiania'),('Goiania', 'Igarassu'), ('Igarassu','Recife')])
+val_map = { 'João Pessoa': 0.9525525,
+            'Campina Grande': 0.815646,
+            'Recife': 0.55739485,
+            'Monteiro': 0.6,
+            'Sertânia': 0.9,
+            'Camalaú': 0.8,
+            'Sumé': 0.7,
             'Zabelê':1.0,
             'São Sebastião do Umbuzeiro': 0.5,
             'Serra Branca': 0.4,
